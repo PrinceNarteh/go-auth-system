1f3fd199f324cee8f6a9da2e982ff1430a076c35
Prince Narteh <prinarteh@gmail.com>
2026-04-26 14:01:23 +0000

    feat: add register and login handlers

---
+++ internal/handlers/auth.handler.go
@@ -0,0 +1,72 @@
+package handlers
+
+import (
+	"errors"
+
+	"auth-system/internal/dtos"
+	"auth-system/internal/repository"
+	"auth-system/internal/services"
+
+	"github.com/gofiber/fiber/v3"
+)
+
+var _ AuthHandler = (*authHandler)(nil)
+
+type AuthHandler interface {
+	Login(fiber.Ctx) error
+	Register(fiber.Ctx) error
+}
+
+type authHandler struct {
+	svc *services.Services
+}
+
+// Register handles user registration
+// POST /api/auth/register
+func (h *authHandler) Register(c fiber.Ctx) error {
+	registerDTO := new(dtos.RegisterDTO)
+	if err := c.Bind().Body(registerDTO); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "invalid request body",
+		})
+	}
+
+	user, err := h.svc.Auth.Register(c.Context(), registerDTO)
+	if err != nil {
+		switch {
+		case errors.Is(err, repository.ErrEmailExists):
+			return c.Status(fiber.StatusConflict).JSON(fiber.Map{
+				"error": "Email already registered",
+			})
+		default:
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": "Failed to register user: " + err.Error(),
+			})
+		}
+	}
+	return c.Status(fiber.StatusCreated).JSON(fiber.Map{
+		"message": "User registered successfully",
+		"user":    user,
+	})
+}
+
+func (h *authHandler) Login(c fiber.Ctx) error {
+	loginDTO := new(dtos.LoginDTO)
+	if err := c.Bind().Body(loginDTO); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "invalid request body",
+		})
+	}
+
+	user, err := h.svc.Auth.Login(c.Context(), loginDTO)
+	if err != nil {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"message": "Login successful",
+		"data":    user,
+	})
+}
---
+++ internal/handlers/handlers.go
@@ -0,0 +1,6 @@
+// Package handlers
+package handlers
+
+type Handlers struct {
+	Auth AuthHandler
+}
